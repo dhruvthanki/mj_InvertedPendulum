818797a546e70a7dc617c228216b641e5436df63
Thanki Dhruv Ashwinkumar <31178720+dhruvthanki@users.noreply.github.com>
2022-05-16 00:29:09 -0400

    mainOSC updated

--- src/mainOSC.py
+++ src/mainOSC.py
@@ -3,7 +3,6 @@ import glfw
 import numpy as np
 np.set_printoptions(precision=4)
 import math
-import cvxpy as cp
 import osqp
 from scipy import sparse
 
@@ -33,11 +32,11 @@ mujoco.mjv_updateScene(
     model, data, mujoco.MjvOption(), mujoco.MjvPerturb(),
     camera, mujoco.mjtCatBit.mjCAT_ALL, scene)
 
-data.qpos = np.array([np.deg2rad(0), np.deg2rad(10)])
+data.qpos = np.array([np.deg2rad(45), np.deg2rad(45)])
 mujoco.mj_forward(model, data)
+
 EndEffector = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_SITE, "EndEffector")
 site_pos = data.site_xpos[EndEffector]
-# print(site_pos)
 ref = np.array([0, 1])
 d_ref = np.zeros(2)
 dd_ref = np.zeros(2)
@@ -50,7 +49,7 @@ st_jacr = np.zeros((3,2))
 mujoco.mj_jacSite(model, data, st_jacp, st_jacr, EndEffector);
 J_old = st_jacp[indices,:]
 Kp = 1000
-Kd = (math.sqrt(Kp)/2) + 200
+Kd = (math.sqrt(Kp)/2) + 60
 M = np.zeros((2,2))
 B = np.identity(2)
 u = np.hstack([ctrl_range, 50*np.ones(2)])
@@ -76,17 +75,6 @@ while(not glfw.window_should_close(window)):
     Aeq = np.block([[-B, M]])
     beq = -Bias
 
-    # CVX_PY
-    # x = cp.Variable(4)
-    # objective = cp.Minimize(cp.sum_squares(a3 @ x - b3))
-    # constraints = [Aeq @ x == beq, x >= -u, x <= u]
-    # prob = cp.Problem(objective, constraints)
-    # try:
-    #     result = prob.solve(verbose=True)
-    # except:
-    #     pass
-    # data.ctrl = x.value[:2]
-
     # OSQP Setup
     # https://scaron.info/blog/conversion-from-least-squares-to-quadratic-programming.html
     Q = (a3.transpose()).dot(a3)
@@ -101,7 +89,6 @@ while(not glfw.window_should_close(window)):
         res = prob.solve()
     except:
         pass
-    # print(res.x[:2])
     data.ctrl = res.x[:2]
 
     mujoco.mj_step2(model, data)
