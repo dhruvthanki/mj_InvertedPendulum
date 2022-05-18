3fb9589a112b158cabd5b614e9a52d49f9e3e525
Thanki Dhruv Ashwinkumar <31178720+dhruvthanki@users.noreply.github.com>
2022-05-18 04:30:29 +0000

    linted

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
+from scipy import sparse
+import osqp
 import mujoco
 import glfw
 import numpy as np
 np.set_printoptions(precision=4)
-import math
-import matplotlib.pyplot as plt
 
 def init_window(max_width, max_height):
     glfw.init()
@@ -31,38 +31,66 @@ mujoco.mjv_updateScene(
     model, data, mujoco.MjvOption(), mujoco.MjvPerturb(),
     camera, mujoco.mjtCatBit.mjCAT_ALL, scene)
 
-Kp = 500
-Kd = (math.sqrt(Kp)/2)
-data.qpos = np.array([np.deg2rad(0), np.deg2rad(10)])
+data.qpos = np.array([np.deg2rad(45), np.deg2rad(45)])
+mujoco.mj_forward(model, data)
 
-dummy_state = np.zeros((1,4))
-
-sensor_states = np.zeros((1,4))
-sensor_states[0,:] = (data.sensordata)
-
-real_states = np.zeros((1,4))
-real_states[0,:] = np.block([[data.qpos, data.qvel]])
-
-time = np.zeros((1,1))
-time[0,0] = data.time
-dummy_time = np.zeros((1,1))
+EndEffector = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_SITE, "EndEffector")
+site_pos = data.site_xpos[EndEffector]
+ref = np.array([0, 1])
+d_ref = np.zeros(2)
+dd_ref = np.zeros(2)
+gear_ratio = model.actuator_gear
+gear_ratio = gear_ratio[:,0]
+ctrl_range = model.actuator_ctrlrange[:,1]
+indices = np.array([0,2])
+st_jacp = np.zeros((3,2))
+st_jacr = np.zeros((3,2))
+mujoco.mj_jacSite(model, data, st_jacp, st_jacr, EndEffector);
+J_old = st_jacp[indices,:]
+Kp = 1000
+Kd = (np.sqrt(Kp)/2) + 60
+M = np.zeros((2,2))
+B = np.identity(2)
+u = np.hstack([ctrl_range, 50*np.ones(2)])
 
 while(not glfw.window_should_close(window)):
     mujoco.mj_step1(model, data)
 
-    data.ctrl = np.zeros(2) + Kd*(np.zeros(2) - data.qvel) + Kp*(np.zeros(2) - data.qpos)
-    # data.ctrl = np.zeros(2) + Kd*(np.zeros(2) - data.sensordata[2:3]) + Kp*(np.zeros(2) - data.sensordata[:2])
+    site_pos = data.site_xpos[EndEffector]
+    xz_pos = site_pos[indices]
+
+    mujoco.mj_jacSite(model, data, st_jacp, st_jacr, EndEffector)
+    Jac = st_jacp[indices,:]
+    dJac = (Jac - J_old)/0.0005
+    J_old = Jac
+
+    a3 = np.block([np.zeros((2, 2)), -Jac])
+    ddy_des = dd_ref + Kd*(d_ref-Jac@data.qvel) + Kp*(ref-xz_pos)
+    b3 = dJac@data.qvel - ddy_des
+
+    mujoco.mj_fullM(model, M, data.qM)
+    Bias = data.qfrc_bias
+
+    Aeq = np.block([[-B, M]])
+    beq = -Bias
+
+    # OSQP Setup
+    # https://scaron.info/blog/conversion-from-least-squares-to-quadratic-programming.html
+    Q = (a3.transpose()).dot(a3)
+    q = -(a3.transpose()).dot(b3)
+    P = sparse.csc_matrix(Q)
+    A = sparse.csc_matrix(Aeq)
+
+    # OSQP
+    prob = osqp.OSQP()
+    prob.setup(P, q, A, beq, beq, verbose=False)
+    try:
+        res = prob.solve()
+    except:
+        pass
+    data.ctrl = res.x[:2]
 
     mujoco.mj_step2(model, data)
-
-    dummy_state[0,:] = (data.sensordata)
-    sensor_states = np.append(sensor_states, dummy_state, axis=0)
-    
-    dummy_state[0,:] = np.block([[data.qpos, data.qvel]])
-    real_states = np.append(real_states, dummy_state, axis=0)
-    
-    dummy_time[0,0] = data.time
-    time = np.append(time, dummy_time, axis=0)
 
     mujoco.mjv_updateScene(
         model, data, mujoco.MjvOption(), None,
@@ -73,11 +101,3 @@ while(not glfw.window_should_close(window)):
     glfw.poll_events()
 
 glfw.terminate()
-
-plt.plot(time[:,0], sensor_states[:,0], color='b', label='sensor')
-plt.plot(time[:,0], real_states[:,0], color='r', label='real')
-plt.xlabel("Time")
-plt.ylabel("Magnitude")
-plt.title("Sine and Cosine functions")
-plt.legend()
-plt.show()
